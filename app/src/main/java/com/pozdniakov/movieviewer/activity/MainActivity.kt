3629020331e412f78fee3aac80802c8806b8c220
Nikita <pozdnyakovnp@gmail.com>
2023-02-05 05:33:09 +0500

    hide status bar

--- app/src/main/java/com/pozdniakov/movieviewer/activity/MainActivity.kt
+++ app/src/main/java/com/pozdniakov/movieviewer/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.pozdniakov.movieviewer.activity
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.pozdniakov.movieviewer.databinding.ActivityMainBinding
 
@@ -9,6 +12,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
